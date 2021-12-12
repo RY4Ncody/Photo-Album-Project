0223b2cdf30173098efc3496154768c4f49bcff0
Ryan <kyawzinhtet6388@gmail.com>
2021-12-12 14:51:25 +0630

    final project

--- register/views.py
+++ register/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import redirect, render
 from .forms import RegisterForm
-
+from django.contrib.auth.models import User
 # Create your views here.
 def register(request):
     if request.method =='POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            
         return redirect('/')
     else:
         form = RegisterForm()
